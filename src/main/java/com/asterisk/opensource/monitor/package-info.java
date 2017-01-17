b64272279165ab82be3e113c3c3917cb96d37153
flaskeer <1185098948@qq.com>
2017-01-17 17:35:50 +0800

    Initial commit

---
+++ src/main/java/com/asterisk/opensource/monitor/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author donghao
+ * @Date 17/1/17
+ * @Time 下午5:34
+ */
+package com.asterisk.opensource.monitor;
---
+++ src/test/java/com/asterisk/opensource/monitor/test/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author donghao
+ * @Date 17/1/17
+ * @Time 下午5:35
+ */
+package com.asterisk.opensource.monitor.test;
